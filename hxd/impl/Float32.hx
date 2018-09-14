9fbe414b37027f7fa2656a253f15e8c6d8023d6e
Valerio Santinelli <santinelli@altralogica.it>
2018-09-14 11:56:27 +0200

    Last step to getting it to compile with lime

--- hxd/impl/Float32.hx
+++ hxd/impl/Float32.hx
@@ -1,3 +1,3 @@
 package hxd.impl;
 
-typedef Float32 = #if cpp cpp.Float32 #elseif hl hl.F32 #else Float #end;
+typedef Float32 = #if lime Float #elseif cpp cpp.Float32 #elseif hl hl.F32 #else Float #end;
